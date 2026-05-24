6bfcd68985e6e7dbaa378ca49d927bef49b4ee3a
WOOJOO KIM <60522969+dnwn0311@users.noreply.github.com>
2026-05-24 20:53:23 +0900

    Revert "Merge branch 'feature/ai-pipeline' into main"
    This reverts commit 019e7281bcd4ff6a8459fc265d7ac6a86a5ba786, reversing
    changes made to bfbe20423762a8403d0d399b77223d7dedada1a8.

--- src/main/java/com/i_route/backend/repository/StudentInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.i_route.backend.repository;
-
-import com.i_route.backend.entity.StudentInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentInfoRepository extends JpaRepository<StudentInfo, String> {
-}
