cbeb68c914b9a2b073ccf90dbb9df1bd23e82642
SeokHee Jung <tjrgml8435@yu.ac.kr>
2026-05-24 19:16:08 +0900

    회원 관리 및 게시판 기능

---
+++ src/main/java/com/i_route/backend/domain/board/entity/Like.java
@@ -0,0 +1,26 @@
+package com.i_route.backend.domain.board.entity;
+
+import com.i_route.backend.domain.user.entity.User;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "likes",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"post_id", "user_id"}))
+@Getter @Setter
+@NoArgsConstructor
+public class Like {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
+
