121a98a8845e0d1b44fc2654d4873d8f0798721c
WOOJOO KIM <60522969+dnwn0311@users.noreply.github.com>
2026-06-05 03:49:59 +0900

    security: 민감한 파일 제거 및 .gitignore 추가 (pem, sql, sh)

--- init.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE DATABASE IF NOT EXISTS i_route_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS 'iroute'@'localhost' IDENTIFIED BY 'iroute_pw';
-GRANT ALL PRIVILEGES ON i_route_db.* TO 'iroute'@'localhost';
-FLUSH PRIVILEGES;
